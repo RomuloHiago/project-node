ab6ec99f9a13ebfef79465cf0c93670056139dc5
Romulo Hiago <102178704+RomuloHiago@users.noreply.github.com>
2024-02-07 14:11:14 -0300

    Update index.js
    Alteração no código para o back-end de outro Project

--- index.js
+++ index.js
@@ -1,106 +1,98 @@
-const express = require ('express')
-const uuid = require('uuid')
+//// BACK DO MEU PROJECT-REACT
 
-const port = 3000
-const app = express ()
-app.use(express.json())
+const express = require('express');  // Importa o módulo 'express' para a variável express
+const uuid = require('uuid'); // Importa o módulo 'uuid' para gerar identificadores únicos
+const cors = require('cors');  // Importa o módulo 'cors' para lidar com políticas de compartilhamento de recursos entre diferentes origens
 
-/*
-        - Query params => meusite.com/users?nome=romulo23&age=28       // FILTROS
-        - Routr params => /users/2       // BUSCAR, DELETAR OU ATUALIZAR ALGO ESPECIFICO
-        - Request body => { "name": "Romulo", "age":}
+const port = 3002;   // Define a porta em que o servidor irá escutar as requisições
+const app = express();  // Inicializa o aplicativo Express
+app.use(express.json());   // Adiciona o middleware para interpretar corpos de solicitação no formato JSON
+app.use(cors());  // Adiciona o middleware para permitir solicitações de origens diferentes (Cross-Origin Resource Sharing)
 
-        - GET          => Buscar informação no back-end
-        - POST         => Criar informaçãoo no back-end
-        - PUT / PATH   => Alterar/Atualixar informação no back-end
-        - DELETE       => Deletar informação no back-end
 
-        -Middleware => INTERCEPITADOR => Tm o poder de parar ou alterar dados da requisição
 
-*/
 
-const users = []
+// Cria um array vazio para armazenar os usuários
+const users = [];
 
-const checkUserId = ( request, response, next) =>{
-   const {id} = request.params 
+                // Define um middleware para verificar se um usuário com o ID especificado existe
+const checkUserId = (request, response, next) => {
+    const { id } = request.params;
+
     
-   const index = users.findIndex( user => user.id === id)
+    const index = users.findIndex(user => user.id === id);  // Procura pelo índice do usuário com o ID especificado no array de usuários
 
-   if(index < 0){
-       return response.status(404).json ({ error: "User not Found"})
-   }
+    
+    if (index < 0) {  // Se o índice for menor que zero, significa que o usuário não foi encontrado
+        return response.status(404).json({ error: "User not Found" });
+    }
 
-   request.userIndex = index
-   request.userId = id
-   next()
-
-}
-
-app.get('/users', (request, response) =>{
-  return response.json(users)
-})
+    // Adiciona o índice do usuário e o ID à requisição para uso posterior
+    request.userIndex = index;
+    request.userId = id;
 
 
-//BUSCANDO INFORMAÇÕES
-app.get('/users', (request, response) => {  // apos o '/' voce coloca o nome que quisr da pasta, obs: toda edição precisa ir la no terminal e rodar novamente 
-        //importante sempre colocar o ':' e apos o nome que deseja nesse caso coloquei ':id'
-    return response.json(users)
-})
+    // Chama a próxima função de middleware na pilha
+    next();
+};
 
+// Rota para buscar todos os usuários
+app.get('/users', (request, response) => {
+    return response.json(users);
+});
 
-
-
-// CRIANDO INFORMAÇÃO
+// Rota para criar um novo usuário
 app.post('/users', (request, response) => {
-    const {name, age} = request.body
+    const { name, age } = request.body;
 
-
- //Alterar/Atualizar informação
-    const user = { id: uuid.v4(), name, age}
-
-    users.push(user)
-
-    return response.status(201).json(user)
-})
-
-
-
-
-
-// ATUALIZAR USUARIO
-app.put('/users/:id',checkUserId, (request, response) => {
-    //const {id} = request.params
-    const { name,age } = request.body
-    const index = request.userIndex
-    const id = request.userId
-
-    const updateUser = {id, name, age}
-
-
-
-    users[index] = updateUser
     
-    return response.json(updateUser)
- 
-})
+    const user = { id: uuid.v4(), name, age }; // Cria um novo usuário com um ID único gerado pelo módulo 'uuid'
 
-app.delete('/users/:id',checkUserId, (request, response) => {
-    const index = request.userIndex
+    
+    users.push(user); // Adiciona o novo usuário ao array de usuários
 
-    users.splice(index,1) 
+    
+    return response.status(201).json(user); // Retorna o novo usuário como resposta com o status 201 (Created)
+});
 
-    return response.status(204).json()
-})
+// Rota para atualizar um usuário existente
+app.put('/users/:id', checkUserId, (request, response) => {
+    const { name, age } = request.body;
+    const index = request.userIndex;
+    const id = request.userId;
+
+    
+    const updateUser = { id, name, age };  // Cria um objeto com os novos dados do usuário
+
+   
+    users[index] = updateUser;   // Substitui o usuário existente pelo usuário atualizado no array de usuários
+
+    
+    return response.json(updateUser); // Retorna o usuário atualizado como resposta
+});
 
 
-/// npm run dev
 
- /* 
-    const{ name, age, } = request.query
-    return response.json({name, age}) // se a chave e o valor for igual pra economizar codigo ({name, age })
-*/
+// Rota para excluir um usuário existente
+app.delete('/users/:id', checkUserId, (request, response) => {
+    const index = request.userIndex;
+
+    
+    users.splice(index, 1);  // Remove o usuário do array de usuários
+
+    
+    return response.status(204).json();  // Retorna uma resposta com o status 204 (No Content) indicando que a operação foi bem-sucedida
+});
 
 
-app.listen (port, () =>{
-    console.log(`🚀server stardet on port ${port}`) //digite Windows tecla do logotipo + . (ponto) pra emoji
-})
+
+// Inicia o servidor na porta especificada e exibe uma mensagem indicando que o servidor está ativo
+app.listen(port, () => {
+    console.log(`🚀server stardet on port ${port}`);
+});
+
+
+
+
+
+
